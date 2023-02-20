8054dc3c97b82eb5f3fabccaf62c8737250b41f3
huwei <1316396297@qq.com>
2023-02-20 22:52:31 +0800

    导入Excel文件

---
+++ StudentManager/DAL/Helper/ImportDataFromExcel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Models;
+using DAL;
+using System.Data;
+
+namespace DAL
+{
+    /// <summary>
+    /// 从Excel中导入数据
+    /// </summary>
+    public class ImportDataFromExcel
+    {
+        /// <summary>
+        /// 将选择的Excel数据表查询后封装成对象集合
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public List<Student> GetStudentByExcel(string path)
+        {
+            List<Student> list = new List<Student>();
+            string sql = string.Format(@"select * from [Student$]");
+            DataSet ds = OleDbHelper.GetDataSet(sql, path);
+            DataTable dt = ds.Tables[0];
+            foreach (DataRow dr in dt.Rows)
+            {
+                list.Add(new Student()
+                {
+                    StudentName = dr["姓名"].ToString(),
+                    Gender = dr["性别"].ToString(),
+                    PhoneNumber = dr["电话号码"].ToString(),
+                    Birthday = Convert.ToDateTime(dr["出生日期"].ToString()),
+                    StudentIdNo = dr["身份证号"].ToString(),
+                    Age = DateTime.Now.Year - Convert.ToDateTime(dr["出生日期"].ToString()).Year,
+                    CardNo = dr["考勤卡号"].ToString(),
+                    StudentAddress = dr["家庭住址"].ToString(),
+                    ClassId = Convert.ToInt32(dr["班级编号"])
+                });
+            }
+            return list;
+        }
+    }
+}
--- StudentManager/DAL/Helper/OleDbHelper.cs
+++ StudentManager/DAL/Helper/OleDbHelper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
 
 namespace DAL
 {
@@ -11,5 +13,96 @@ namespace DAL
     /// </summary>
     public class OleDbHelper
     {
+        //创建连接字符串
+        private static string connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=Excel 8.0";
+
+        public static int Update(string sql)
+        {
+            OleDbConnection conn = new OleDbConnection(connString);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static object GetSingleResult(string sql)
+        {
+            OleDbConnection conn = new OleDbConnection(connString);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            } 
+        }
+
+        public static OleDbDataReader GetReader(string sql)
+        {
+            OleDbConnection conn = new OleDbConnection(connString);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        /// <summary>
+        /// 将指定路径的excel导入到数据集中
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static DataSet GetDataSet(string sql, string path)
+        {
+            OleDbConnection conn = new OleDbConnection(string.Format(connString, path));
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            try
+            {
+                conn.Open();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 }
