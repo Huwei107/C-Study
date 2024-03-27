b12ee24c59f091de55b4380da2ea85b462f1b1af
Huwei107 <1316396297@qq.com>
2024-03-27 23:42:42 +0800

    20240327

---
+++ AspNetCoreProject/SwiftCode.BBS.API/SwiftCode.BBS.API/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwiftCode.BBS.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v0.1.0",
+                    Title = "SwiftCode.BBS.API",
+                    Description = "框架说明文档",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "SwiftCode",
+                        Email = "1316396297@qq.com",
+                    }
+                });
+            });
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                c.RoutePrefix = "";
+            });
+            #endregion
+
+            //路由中间件
+            app.UseRouting();
+
+            //授权中间件
+            app.UseAuthorization();
+
+            //短路中间件，表示http请求到这里就停止了
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
---
+++ AspNetCoreProject/SwiftCode.BBS.API/SwiftCode.BBS.Serivces/CalculateSerivce.cs
@@ -0,0 +1,14 @@
+﻿using SwiftCode.BBS.IServices;
+using System;
+
+namespace SwiftCode.BBS.Services
+{
+    public class CalculateService : ICalculateService
+    {
+        ICalculateService _calculateRepository = new CalculateService();
+        public int Sum(int i,int j)
+        {
+            return _calculateRepository.Sum(i,j);
+        }
+    }
+}
