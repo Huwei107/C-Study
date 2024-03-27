781e2037d93454e547c02525cd29b0d7a77189be
huwei <1316396297@qq.com>
2024-03-27 17:36:44 +0800

    单例模式

---
+++ TestDemo/单例模式test/单例模式test/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+
+
+
+using 单例模式test;
+
+Singleton1 singleton1 = Singleton1.GetInstance();
+Singleton1 singleton2 = Singleton1.GetInstance();
+
+
+
+Console.WriteLine($"{singleton1}");
+Console.WriteLine($"{singleton2}");
+
+
+
