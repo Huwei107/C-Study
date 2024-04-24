1c3bb18a635a28912be22797e1e09932aaa38a24
Huwei107 <1316396297@qq.com>
2024-04-25 00:24:52 +0800

    登录基本实现

--- AspNetCoreProject/WPF/OAManage/OAManage/MainWindow.xaml.cs
+++ AspNetCoreProject/WPF/OAManage/OAManage/MainWindow.xaml.cs
@@ -23,6 +23,39 @@ namespace OAManage
         public MainWindow()
         {
             InitializeComponent();
+
+            //设置数据上下文
+            this.DataContext = this;
+        }
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string Account { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 登录，单击执行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Login(object sender, RoutedEventArgs e)
+        {
+            //前后耦合大
+            //string account = txtAccount.Text;
+            //string password = txtPassword.Password;
+            if(Account == "admin" && Password == "123")
+            {
+                MessageBox.Show("登录成功！");
+            }
+            else
+            {
+                MessageBox.Show("登录失败！");
+                //清空账号密码
+                Password = string.Empty;//后台清空
+            }
         }
     }
 }
