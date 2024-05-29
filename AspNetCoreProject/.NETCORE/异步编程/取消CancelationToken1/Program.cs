4652369c2a679ed489870113c8da062610e556d2
huwei <1316396297@qq.com>
2024-05-29 17:24:46 +0800

    CancellationToken

---
+++ AspNetCoreProject/.NETCORE/异步编程/取消CancelationToken1/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+
+
+using System.Diagnostics;
+
+class Program
+{
+    private static async Task Main(string[] args)
+    {
+        try
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            //1秒后不结束就直接结束
+            cts.CancelAfter(1000);
+            await DownloadAsync3("https://www.baidu.com/", 100, cts.Token);
+        }
+        catch (TaskCanceledException ex)
+        {
+            Console.WriteLine($"{DateTime.Now}：{ex.Message}请求被取消！");
+        }
+    }
+
+    static async Task DownloadAsync1(string url, int n)
+    {
+        using (HttpClient clent = new HttpClient())
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var html = await clent.GetStringAsync(url);
+                Console.WriteLine($"{DateTime.Now}：{html}");
+            }
+        }
+    }
+
+    static async Task DownloadAsync2(string url, int n, CancellationToken cancellationToken)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        stopwatch.Start();
+        using (HttpClient clent = new HttpClient())
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                var html = await clent.GetStringAsync(url);
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine($"{DateTime.Now}：请求被取消！");
+                    break;
+                }
+                Console.WriteLine($"{DateTime.Now}：{html}");
+            }
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"共耗时：{stopwatch.ElapsedMilliseconds}");
+    }
+
+    /// <summary>
+    /// 这种方法更好，主不过要自己捕获异常来处理
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="n"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    static async Task DownloadAsync3(string url, int n, CancellationToken cancellationToken)
+    {
+        using (HttpClient clent = new HttpClient())
+        {
+            for (int i = 0; i < n; i++)
+            {
+                var responseMessage = await clent.GetAsync(url, cancellationToken);//抛出异常来终止
+                var html = responseMessage.Content.ReadAsStringAsync();
+                Console.WriteLine($"{DateTime.Now}：{html}");
+            }
+        }
+    }
+}
+
+
--- TestDemo/单例模式test/单例模式test/Program.cs
+++ TestDemo/单例模式test/单例模式test/Program.cs
@@ -21,7 +21,7 @@ Console.WriteLine($"{singleton2}");
 
 Console.WriteLine($"===================差集======================");
 
-var list1 = new List<int>() { 1,2, 3 };
+var list1 = new List<int>() { 1, 2, 3 };
 var list2 = new List<int>() { 1, 2, 4 };
 
 var res1 = list1.Except(list2).ToList();
@@ -34,5 +34,18 @@ Console.WriteLine($"====================测试=====================");
 var num1 = 3.0;
 string result = num1 % 1 == 0 ? "整数" : "小数";
 Console.WriteLine($"{result}");
+
+Console.WriteLine($"====================位运算=====================");
+
+int a = 13;
+int b = 20;
+//a ^= b;
+//b ^= a;
+//a ^= b;
+a = ~a;
+Console.WriteLine($"a:{a}   b:{b}");
+
+
+
 
 
