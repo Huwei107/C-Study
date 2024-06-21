ebe488792cd33dc01b91e2966abf237b7490252e
huwei <1316396297@qq.com>
2024-06-21 10:16:37 +0800

    饿汉式单例

---
+++ TestDemo/单例模式test/单例模式test/EagerSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 单例模式test
+{
+    /// <summary>
+    /// 饿汉式单例模式
+    /// 1-4都是懒汉式单例
+    /// </summary>
+    public class EagerSingleton
+    {
+        //先把这个实例创建出来
+        private static EagerSingleton instance = new EagerSingleton();
+
+        //私有的构造方法
+        private EagerSingleton() { }
+
+        // 如果要使用这个实例，统一通过 Singleton.getInstance() 方式获取
+        // 饿汉模式只涉及到读操作，因此是线程安全的
+        public static EagerSingleton getInstance()
+        {
+            return instance;
+        }
+    }
+}
